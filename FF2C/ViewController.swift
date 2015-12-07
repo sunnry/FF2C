18e12b7f9403d2ef96c3f9d760a5e97d291291b6
sunnry <sunhaihuan@gmail.com>
2015-12-07 14:58:52 +0800

    Initial commit
    Initial version commit

---
+++ FF2C/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FF2C
+//
+//  Created by sunny sun on 15/12/7.
+//  Copyright © 2015年 sunny sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
